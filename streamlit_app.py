df35f65320d6f35ba3c79be9432b23974528b6c9
ryguycode <147287166+ryguycode@users.noreply.github.com>
2023-10-07 21:34:49 -0400

    Update streamlit_app.py
    Comment out new code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv=("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = pandas.read_csv=("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
@@ -10,4 +10,4 @@ streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
