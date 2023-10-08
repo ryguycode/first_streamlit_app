8046dc9809a582647340546144252cb5ece17c97
ryguycode <147287166+ryguycode@users.noreply.github.com>
2023-10-07 21:32:52 -0400

    Update streamlit_app.py
    Created syntax for emojis to be included within quotes.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ my_fruit_list = pandas.read_csv=("https://uni-lab-files.s3.us-west-2.amazonaws.c
 streamlit.dataframe(my_fruit_list)
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞'Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
