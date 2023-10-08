1e5db0ff76e53747580e9a0888b2873045dcc5a3
ryguycode <147287166+ryguycode@users.noreply.github.com>
2023-10-07 22:20:14 -0400

    Update streamlit_app.py
    corrected typo on line 18 streamlit.dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #display the table on the page
-stramlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
