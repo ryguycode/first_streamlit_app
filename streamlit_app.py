6a0867a39129a80d28a04af7e25cfaf1de4022ef
ryguycode <147287166+ryguycode@users.noreply.github.com>
2023-10-07 21:50:14 -0400

    Update streamlit_app.py
    attempting to remove offending line.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 import pandas
 
-#my_fruit_list = pandas.read_csv=("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#streamlit.dataframe(my_fruit_list)
+
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
