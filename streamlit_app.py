1a2bb703e86eae38f63c262083dd0f7b9f7036c3
ryguycode <147287166+ryguycode@users.noreply.github.com>
2023-11-05 01:47:16 -0400

    Update streamlit_app.py
    fix input text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,6 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-streamlit.header("What fruit would you like to add?")
-add_my_fruit = streamlit.text_input
+
+add_my_fruit = streamlit.text_input(What fruit would you like to add?, 'Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
